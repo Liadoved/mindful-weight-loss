066160067f736bc9e4f3c59c06ff93206633383d
Liad Oved <your.email@example.com>
2025-02-10 13:14:11 +0200

    עדכון המיגרציה להעתקת נתונים מ-full_name ל-name

--- migrations/versions/add_name_column.py
+++ migrations/versions/add_name_column.py
@@ -1,7 +1,7 @@
 """Add name column
 
 Revision ID: add_name_column
-Create Date: 2025-02-10 12:59:00.000000
+Create Date: 2025-02-10 13:12:00.000000
 
 """
 from alembic import op
@@ -16,7 +16,25 @@ depends_on = None
 def upgrade() -> None:
     # Add name column if it doesn't exist
     op.add_column('user', sa.Column('name', sa.String(120)))
+    
+    # Copy data from full_name to name if full_name exists
+    try:
+        op.execute('UPDATE "user" SET name = full_name')
+    except:
+        pass
+    
+    # Drop full_name column if it exists
+    try:
+        op.drop_column('user', 'full_name')
+    except:
+        pass
 
 def downgrade() -> None:
-    # Remove name column
+    # Add full_name column
+    op.add_column('user', sa.Column('full_name', sa.String(120)))
+    
+    # Copy data from name to full_name
+    op.execute('UPDATE "user" SET full_name = name')
+    
+    # Drop name column
     op.drop_column('user', 'name')
