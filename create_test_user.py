0e3a54bd16cca970f5f05ea031776a3cd9109871
Liad Oved <your.email@example.com>
2025-02-11 17:20:17 +0200

    הוספת משתמש אדמין נוסף

--- create_test_user.py
+++ create_test_user.py
@@ -2,18 +2,31 @@ from app import app, db, User
 from werkzeug.security import generate_password_hash
 from datetime import datetime, timezone
 
-# יצירת משתמש אדמין
-admin_user = User(
-    username='admin',
-    email='admin@razit.co.il',
-    password_hash=generate_password_hash('Razit123321'),
-    full_name='מנהל המערכת',
-    phone='',
-    gender='other',
-    registration_date=datetime.now(timezone.utc),
-    difficulty=0,
-    is_admin=True
-)
+# יצירת משתמשי אדמין
+admin_users = [
+    User(
+        username='admin',
+        email='admin@razit.co.il',
+        password_hash=generate_password_hash('Razit123321'),
+        full_name='מנהל המערכת',
+        phone='',
+        gender='other',
+        registration_date=datetime.now(timezone.utc),
+        difficulty=0,
+        is_admin=True
+    ),
+    User(
+        username='razit.mindful',
+        email='razit.mindful@gmail.com',
+        password_hash=generate_password_hash('Razit123321'),
+        full_name='מנהל המערכת',
+        phone='',
+        gender='other',
+        registration_date=datetime.now(timezone.utc),
+        difficulty=0,
+        is_admin=True
+    )
+]
 
 with app.app_context():
     # יצירת הטבלאות
@@ -22,12 +35,16 @@ with app.app_context():
     db.create_all()  # יוצר את כל הטבלאות מחדש
     print("Database tables created successfully!")
 
-    # בדיקה אם המשתמש כבר קיים
-    existing_admin = User.query.filter_by(username='admin').first()
-    if existing_admin is None:
-        # הוספת המשתמש לבסיס הנתונים
-        db.session.add(admin_user)
-        db.session.commit()
-        print("Admin user created successfully!")
-    else:
-        print("Admin user already exists!")
+    # הוספת המשתמשים
+    for admin_user in admin_users:
+        existing_admin = User.query.filter(
+            (User.username == admin_user.username) | 
+            (User.email == admin_user.email)
+        ).first()
+        
+        if existing_admin is None:
+            db.session.add(admin_user)
+            db.session.commit()
+            print(f"Admin user {admin_user.username} created successfully!")
+        else:
+            print(f"Admin user {admin_user.username} already exists!")
